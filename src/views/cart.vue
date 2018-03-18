ba6bdf9432408b402c5da00c7000ef1a5d8b3815
ysmull <ysmull@aliyun.com>
2018-03-19 01:37:36 +0800

    修复连续两个Modal无法连续切换的iView组件bug

--- src/views/cart.vue
+++ src/views/cart.vue
@@ -5,6 +5,14 @@
       <span class="amount" style="text-align">总计：¥{{ this.amount }}</span>
     </div>
     <Button @click="purchase">购买</Button>
+    <Modal
+        v-model="confirmBuy"
+        :loading="buyloading"
+        title="确认"
+        @on-ok="asyncOK"
+        :width="300">
+        <p>确认购买所选物品？</p>
+    </Modal>
 </div>
 </template>
 <style scoped>
@@ -28,6 +36,7 @@ export default {
   data() {
     return {
       amount: 0,
+      confirmBuy: false,
       columns: [
         {
           type: "selection",
@@ -43,8 +52,8 @@ export default {
               "router-link",
               {
                 props: {
-                  to : {
-                    name: 'goods',
+                  to: {
+                    name: "goods",
                     params: {
                       goodsId: params.row.goodsId
                     }
@@ -71,8 +80,8 @@ export default {
               "router-link",
               {
                 props: {
-                  to : {
-                    name: 'goods',
+                  to: {
+                    name: "goods",
                     params: {
                       goodsId: params.row.goodsId
                     }
@@ -149,49 +158,50 @@ export default {
       ],
       cartList: [],
       loading: true,
-      modalLoading: true
+      buyloading: false
     };
   },
   methods: {
     remove(row) {
-      let that = this
-      this.$http.post("/deletecart", {
+      let that = this;
+      this.$http
+        .post("/deletecart", {
           userId: this.$store.state.userId,
           goodsId: row.goodsId
         })
         .then(response => {
           this.$Message.success("删除成功");
           that.cartList = that.cartList.filter(item => {
-            return item.goodsId != row.goodsId
-          })
+            return item.goodsId != row.goodsId;
+          });
         })
         .catch(error => {
           this.$Message.error("删除失败");
         });
     },
     selectionChange() {
-      this.amount = this.$refs.cartGoods.getSelection().reduce((total, item) => {return total + item.price*item.amount}, 0)
+      this.amount = this.$refs.cartGoods
+        .getSelection()
+        .reduce((total, item) => {
+          return total + item.price * item.amount;
+        }, 0);
+    },
+    asyncOK() {
+      this.$http
+        .post("/buy", this.$refs.cartGoods.getSelection())
+        .then(response => {
+          this.$Message.success({
+            content: "购买成功",
+            dureation: 1
+          });
+          setTimeout(() => {
+            location.reload();
+          }, 100);
+        });
     },
     purchase() {
       if (this.$refs.cartGoods.getSelection().length > 0) {
-        this.$Modal.confirm({
-          title: "确认购买所选物品？",
-          content: "",
-          loading: "modalLoading",
-          onOk: () => {
-            this.$http.post("/buy", this.$refs.cartGoods.getSelection())
-              .then(response => {
-                this.$Message.success({
-                  content: "购买成功",
-                  dureation: 1
-                });
-                setTimeout(() => {
-                  location.reload();
-                }, 500);
-              });
-          },
-          onCancel: () => {}
-        });
+        this.confirmBuy = true;
       } else {
         this.$Modal.warning({
           title: "提示",
@@ -202,8 +212,7 @@ export default {
   },
   mounted() {
     console.log("cart mounted!");
-    this.$http.get("/getcart")
-    .then(response => {
+    this.$http.get("/getcart").then(response => {
       this.cartList = response.data.data;
       this.loading = false;
     });
