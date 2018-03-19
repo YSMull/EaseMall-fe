78b9ca5562ea6b5af8f270f4155996f99500114d
ysmull <ysmull@aliyun.com>
2018-03-19 16:03:50 +0800

    由于publish页面在刚进入的时候没有任何api请求操作 导致无法确定是否有权限进入该页面 因此使用「硬鉴权」，主动触发validation。

--- src/views/publish.vue
+++ src/views/publish.vue
@@ -278,17 +278,15 @@ export default {
     }
   },
   watch: {
-    // 如果路由有变化，会再次执行该方法
     '$route': 'fetchData'
   },
+  // 需要路由硬鉴权时采用
+  beforeRouteEnter(to, from, next) {
+      Vue.prototype.$validate(to, from, next);
+  },
   mounted() {
-    if (!this.$isLogin()) {
-        this.$router.push({
-            name: 'home'
-        });
-    }
-    this.fetchData()
     console.log('publish mounted');
+    this.fetchData();
   }
 };
 </script>
